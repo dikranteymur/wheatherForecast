4d72a3ffff7649363536c367108fd904adbe0492
dikranteymur <mdteymur@gmail.com>
2023-11-08 12:31:56 +0300

    Edited file system

---
+++ Wheather Forecast/Wheather Forecast/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Wheather Forecast
+//
+//  Created by Dikran Teymur on 8.11.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
