e491b209c9c1f3eabe35453f3ee04139d09d51fc
dikranteymur <mdteymur@gmail.com>
2023-11-08 13:42:38 +0300

    Pod files installed

---
+++ Wheather Forecast/Pods/Target Support Files/Pods-Wheather Forecast-Wheather ForecastUITests/Pods-Wheather Forecast-Wheather ForecastUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wheather_Forecast_Wheather_ForecastUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Wheather_Forecast_Wheather_ForecastUITests
+@end
---
+++ Wheather Forecast/Pods/Target Support Files/Pods-Wheather ForecastTests/Pods-Wheather ForecastTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Wheather_ForecastTests : NSObject
+@end
+@implementation PodsDummy_Pods_Wheather_ForecastTests
+@end
